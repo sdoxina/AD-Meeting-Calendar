ccc7d5d0b6ba6247a7fe59c3db89ba8e0052d24b
sdoxina <202311026@fit.edu.ph>
2025-06-24 23:25:41 +0800

    feat: create meeting_users table for managing user roles in meetings

---
+++ database/meeting_users.model.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS meeting_users (
+    meeting_id INTEGER NOT NULL REFERENCES meetings(id),
+    user_id INTEGER NOT NULL REFERENCES users(id),
+    role VARCHAR(50) NOT NULL,
+    PRIMARY KEY (meeting_id, user_id)
+);
