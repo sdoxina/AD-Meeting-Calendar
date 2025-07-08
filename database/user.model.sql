297b4bcf17e3798f7bece1ffe56b18a42af55366
sdoxina <202311026@fit.edu.ph>
2025-07-08 21:21:20 +0800

    fix: ensure users table is dropped before recreation in user model

--- database/user.model.sql
+++ database/user.model.sql
@@ -1,9 +1,11 @@
-CREATE TABLE IF NOT EXISTS public."users" (
-    id                  uuid            NOT NULL PRIMARY KEY DEFAULT gen_random_uuid (),
-    first_name          varchar(225)    NOT NULL,
-    middle_name         varchar(225),
-    last_name           varchar(225)    NOT NULL,
-    password            varchar(225)    NOT NULL,
-    username            varchar(225)    NOT NULL UNIQUE,
-    role                varchar(225)    NOT NULL
+DROP TABLE IF EXISTS public.users CASCADE;
+
+CREATE TABLE public.users (
+    id uuid PRIMARY KEY DEFAULT gen_random_uuid (),
+    first_name varchar(225) NOT NULL,
+    middle_name varchar(225),
+    last_name varchar(225) NOT NULL,
+    password varchar(225) NOT NULL,
+    username varchar(225) NOT NULL UNIQUE,
+    role varchar(225) NOT NULL
 );
