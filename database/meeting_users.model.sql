5060a96ad30f8a21a8b34f85f134455b3377a505
sdoxina <202311026@fit.edu.ph>
2025-07-08 21:38:07 +0800

    fix: change user_id type from INTEGER to UUID in meeting_users table

--- database/meeting_users.model.sql
+++ database/meeting_users.model.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS meeting_users (
     meeting_id INTEGER NOT NULL REFERENCES meetings(id),
-    user_id INTEGER NOT NULL REFERENCES users(id),
+    user_id UUID NOT NULL REFERENCES users(id),
     role VARCHAR(50) NOT NULL,
     PRIMARY KEY (meeting_id, user_id)
 );
